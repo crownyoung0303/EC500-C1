57498ebc2c1e05e5dadd002fe1ed73174a963d0a
Lingshan Yang <31738013+crownyoung0303@users.noreply.github.com>
2018-02-06 15:06:20 -0500

    v2

--- HW1/homework1.py
+++ HW1/homework1.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 # encoding: utf-8
-#Author - Prateek Mehta
-
 
 import tweepy #https://github.com/tweepy/tweepy
 import json
 import wget
+import sys
+import io
+import subprocess
+import glob
 
 #Twitter API credentials
-consumer_key = "input your consumer key"
-consumer_secret = "input your consumer key"
-access_key = "input your consumer key"
-access_secret = "input your consumer key"
+consumer_key = 
+consumer_secret = 
+access_key = 
+access_secret = 
 
 
 def get_all_tweets(screen_name):
-    
-    #Twitter only allows access to a users most recent 3240 tweets with this method
-    
     #authorize twitter, initialize tweepy
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
@@ -58,17 +57,94 @@ def get_all_tweets(screen_name):
     #close the file
     print ("Done")
     file.close()'''
-    
+    # get the image urls
     media_files = set()
     for status in alltweets:
         media = status.entities.get('media', [])
         if(len(media) > 0):
             media_files.add(media[0]['media_url'])
+    
+    # download the images
+    i = 1
     for media_file in media_files:
-        wget.download(media_file)
+        wget.download(media_file,'image'+str(i)+'.jpg')
+        i += 1
     print("Images downloading done.")
+    
+    #os.system('ffmpeg -y -framerate 20 -i d:/graduate/ec500c1/image%d.JPG -pix_fmt yuv420p -filter:v "setpts=5.0*PTS" a.mp4')
+    #os.system('PAUSE)
 
+def ffmpeg():
+    try: 
+        subprocess.call('ffmpeg -y -framerate 20 -i image%d.JPG -pix_fmt yuv420p -filter:v "setpts=5.0*PTS" output.mp4')
+    except (RuntimeError, TypeError,NameError):
+        print("Can not create valid video.")
+        pass
+
+
+def googlelabels(description_count = 2):
+	client = google.cloud.vision.ImageAnnotatorClient()
+
+	outputdesc = {}
+
+	for filename in glob.glob('*.jpg'):
+		image_file_name = filename
+		with io.open(image_file_name, 'rb') as image_file:
+			content = image_file.read()
+
+		# Use Vision to label the image based on content.
+		image = google.cloud.vision.types.Image(content=content)
+		response = client.label_detection(image=image)
+
+		image_desc = []
+		for label in response.label_annotations:
+			if len(image_desc) < description_count:
+				features = {}
+				features['mid'] = label.mid
+				features['description'] = label.description 
+				features['score'] = str(label.score)
+				features['topicality'] = str(label.topicality)
+				image_desc.append(features)
+			else:
+				break
+		if len(image_desc) == None:
+			print ("There are no labels.")
+		else:
+			outputdesc[filename] = image_desc
+	with open('labels.json','w') as outfile:
+		json.dump(outputdesc, outfile, indent = 4, sort_keys = True)
+
+	return outfile
+
+def a():
+
+    # Instantiates a client
+    GOOGLE_APPLICATION_CREDENTIALS = './ec500-hw1-cf9544334042.json'
+    client = vision.ImageAnnotatorClient()
+    
+    # The name of the image file to annotate
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        'image2.jpg')
+    
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+    
+    image = types.Image(content=content)
+    
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    
+    print('Labels:')
+    for label in labels:
+        print(label.description)
 
 if __name__ == '__main__':
-    #pass in the username of the account you want to download
+    # pass in the username of the account you want to download
     get_all_tweets("@ESPNNBA")
+    ffmpeg()
+    #googlelabels()
+    #a()
+    
